513e6a0248886597689ceb9b89171175fc3aa455
Vinay288 <vinayhiremath288@gmail.com>
2021-10-07 00:21:04 +0530

    MODIFIED: array to store employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -11,12 +11,16 @@ function employeeAttendence() {
         console.log("Employee is Present");
     }
 }
+let employeeWageArray=new Array()
+function getEmployeeWage(employeeHours){
+employeeWageArray.push(employeeHours*WAGE_PER_HOUR)
+}
+
 function getWorkingHours() {
     const PART_TIME = 0;
     const FULL_TIME = 1;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
-
     let employeeHours = 0;
     let employeeValue = Math.floor(Math.random() * 10) % 2;
     switch (employeeValue) {
@@ -29,15 +33,18 @@ function getWorkingHours() {
         default:
             employeeHours = 0;
     }
+    getEmployeeWage(employeeHours)
     return employeeHours;
 }
 const MAXIMUM_WORKING_DAYS = 20;
 const WAGE_PER_HOUR = 20;
 const MAXIMUM_WOKING_HOURS = 160;
 let employeeWage = 0, workingHours = 0, day = 0;
+
 for (day = 0; day < MAXIMUM_WORKING_DAYS && workingHours < 160; day++) {
     workingHours += getWorkingHours();
 
 }
 employeeWage += workingHours * WAGE_PER_HOUR;
+console.log("employee wage array "+employeeWageArray)
 console.log("Employee wage = " + employeeWage + " working hours = " + workingHours + " days = " + day);
