1167c8363c3055c0ada69f989947601fa0856127
Vinay288 <vinayhiremath288@gmail.com>
2021-10-13 02:00:38 +0530

    ADDED: method to reset all fields on pressing reset

--- Js/employee_payroll.js
+++ Js/employee_payroll.js
@@ -25,57 +25,87 @@ function onSubmit() {
   // console.log(employeeData);
 }
 const save = () => {
-  try{
+  try {
     let employeePayrollData = createEmployeePayroll();
     createUpdateLocalStorage(employeePayrollData);
-  }catch(e){
+  } catch (e) {
     return;
   }
 }
-const createEmployeePayroll = () =>{
+const createEmployeePayroll = () => {
   let employeePayrollData = new EmployeePayrollData();
-  try{
+  try {
     employeePayrollData.name = getInputValueById('#name');
-  }catch(e){
-    setTextValue('.text-error',e);
+  } catch (e) {
+    setTextValue('.text-error', e);
     throw e;
   }
   employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
   employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
   employeePayrollData.department = getSelectedValues('[name=department]');
   employeePayrollData.salary = getInputValueById('#salary');
-  employeePayrollData.note= getInputValueById('#notes');
-  let date = getInputValueById('#month')+" "+ getInputValueById('#day')+", "+
-            getInputValueById('#year');
-            console.log(date)
-  employeePayrollData.startDate=new Date(date);
+  employeePayrollData.note = getInputValueById('#notes');
+  let date = getInputValueById('#month') + " " + getInputValueById('#day') + ", " +
+    getInputValueById('#year');
+  console.log(date)
+  employeePayrollData.startDate = new Date(date);
   alert(employeePayrollData.toString());
   return employeePayrollData;
 }
-const getSelectedValues = (propertyValue) =>{
+const getSelectedValues = (propertyValue) => {
   let allItems = document.querySelectorAll(propertyValue);
   let selItems = [];
   allItems.forEach(item => {
-    if(item.checked) selItems.push(item.value);
+    if (item.checked) selItems.push(item.value);
   });
   return selItems;
 }
-const getInputValueById =(id) =>{
-  let value =document.querySelector(id).value;
+const getInputValueById = (id) => {
+  let value = document.querySelector(id).value;
   return value;
 }
 
-function createUpdateLocalStorage(employeePayrollData) 
-{
-    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
-    if (employeePayrollList != undefined) 
-    {
-        employeePayrollList.push(employeePayrollData);
-    } 
-    else 
-    {
-        employeePayrollList = [employeePayrollData];
-    }
-    alert("Local Storage Updated Successfully!\nTotal Employees : " + employeePayrollList.length);
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+function createUpdateLocalStorage(employeePayrollData) {
+  let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+  if (employeePayrollList != undefined) {
+    employeePayrollList.push(employeePayrollData);
+  }
+  else {
+    employeePayrollList = [employeePayrollData];
+  }
+  alert("Local Storage Updated Successfully!\nTotal Employees : " + employeePayrollList.length);
+  localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
+const resetForm = () => {
+  setValue("#name", "");
+  unsetSelectedValues("[name=profile]");
+  unsetSelectedValues("[name=gender]");
+  unsetSelectedValues("[name=department]");
+  setValue("#day", "1");
+  setValue("#month", "January");
+  setValue("#year", "2020");
+  setValue("#notes", "");
+  resetRange("#salary", ".salary-output");
+};
+
+const setValue = (propertyId, value) => {
+  const element = document.querySelector(propertyId);
+  element.value = value;
+};
+
+const unsetSelectedValues = (propertyName) => {
+  let allValues = document.querySelectorAll(propertyName);
+  allValues.forEach(input => input.checked == false);
+};
+
+const setTextValue = (propertyId, value) => {
+  const element = document.querySelector(propertyId);
+  element.textContent = value;
+};
+
+const resetRange = (propertyId, outputId) => {
+  const rangeElement = document.querySelector(propertyId);
+  rangeElement.value = 400000;
+  const outputElement = document.querySelector(outputId);
+  outputElement.textContent = rangeElement.value;
+};
