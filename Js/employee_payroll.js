9d0da7e1d9df6033f9e5b9042cd05ddedd1335a6
Vinay288 <vinayhiremath288@gmail.com>
2021-10-12 21:48:50 +0530

    MODIFED: event isteners for name and salary

--- Js/employee_payroll.js
+++ Js/employee_payroll.js
@@ -1,5 +1,26 @@
-var salaryOutput = document.querySelector(".salary-output");
-var salary = document.querySelector("#salary");
-salary.addEventListener("input", function () {
-  salaryOutput.textContent = salary.value;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  const name = document.querySelector('#name');
+  const textError = document.querySelector('.text-error');
+  name.addEventListener('input', function () {
+    if (name.value.length == 0) {
+      textError.textContent = "";
+      return;
+    }
+    try {
+      (new EmployeePayrollData()).name = name.value;;
+      textError.textContent = "";
+    } catch (e) {
+      textError.textContent = e;
+    }
+  });
+  var salaryOutput = document.querySelector(".salary-output");
+  var salary = document.querySelector("#salary");
+  salary.addEventListener("input", function () {
+    salaryOutput.textContent = salary.value;
+  });
 });
+function onSubmit() {
+  let employeeData = new EmployeePayrollData(document.getElementById('name').value, document.getElementById('salary').value, document.getElementById('gender').value, document.getElementById('startDate'))
+  // console.log(employeeData);
+}
