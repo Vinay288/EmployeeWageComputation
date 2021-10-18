c24ab868573e9c3969596abff07bc77b1f4414ca
Vinay288 <vinayhiremath288@gmail.com>
2021-10-18 20:48:12 +0530

    ADDED: validataion methods

--- Js/site_properties.js
+++ Js/site_properties.js
@@ -1,4 +1,6 @@
 let site_properties={
+    localStorage:"true",
     home_page:"../Pages/employee_payroll_home.html",
-    add_emp_payroll_page:"../Pages/employee_payroll.html"
+    add_emp_payroll_page:"../Pages/employee_payroll.html",
+    site_url:"http://127.0.0.1:3000/employees/"
 }
