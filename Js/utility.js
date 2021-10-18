edbc9c56bed4f26b3fa83006e7283877b230253e
Vinay288 <vinayhiremath288@gmail.com>
2021-10-18 20:47:41 +0530

    ADDED: site url and local storage attributes

--- Js/utility.js
+++ Js/utility.js
@@ -1,6 +1,22 @@
 const stringifyDate = (date) => {
-    const options = { day: 'numeric',month: 'short', year: 'numeric' };
+    const options = { day: 'numeric', month: 'short', year: 'numeric' };
     const newDate = !date ? "undefined" :
-                    new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
+        new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
     return newDate;
+}
+const checkName = (name) => {
+    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z ]{3,}$');
+    if (!nameRegex.test(name)) {
+        throw "Name is Incorrect";
+    }
+    else
+        return true;
+}
+
+const checkStartDate = (startDate) => {
+    const days = Math.abs(startDate - new Date()) / (1000 * 60 * 60 * 24);
+    if ((startDate <= new Date()) && days < 30)
+        return true;
+    else throw 'Invalid date';
+
 }
