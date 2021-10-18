4d714fa68f7b5fbdc020ec3f0ac39a689f1b5298
Vinay288 <vinayhiremath288@gmail.com>
2021-10-18 23:33:31 +0530

    MODIFIED: site url

--- Js/site_properties.js
+++ Js/site_properties.js
@@ -1,6 +1,6 @@
 let site_properties={
-    localStorage:"true",
+    localStorage:"false",
     home_page:"../Pages/employee_payroll_home.html",
     add_emp_payroll_page:"../Pages/employee_payroll.html",
-    site_url:"http://127.0.0.1:3000/employees/"
+    site_url:"http://127.0.0.1:3000/EmployeePayrollDB/"
 }
